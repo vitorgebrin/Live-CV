5a53d21cafee1f588c745a7a8e7a5786a33a9e17
vitorgebrin <vitorgebrin@gmail.com>
2024-08-09 23:01:03 +0200

    changed the menu and added new QuemSou section

--- src/App.js
+++ src/App.js
@@ -8,93 +8,83 @@ import textos from "./json/Textos.json"
 import './App.css'
 import './index.css'
 
-import { Container, Flex,Button,useColorModeValue,useColorMode } from '@chakra-ui/react';
-import { NotAllowedIcon,SunIcon,MoonIcon } from '@chakra-ui/icons'
+import { Container, Flex,Button,useColorModeValue,useColorMode,useDisclosure } from '@chakra-ui/react';
+import { HamburgerIcon,SunIcon,MoonIcon } from '@chakra-ui/icons'
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from '@chakra-ui/react'
 
 function App() {
-  const [darkOn, setDarkOn] = React.useState(false)
-  const [lang, setLang] = React.useState('en')
-  var paginas = { 'home': <QuemSou idioma={lang} />, 'habilidades': <Habilidades idioma={lang} />, 'Experiencias': <Experiencias idioma={lang} darkmode={darkOn} /> } //defining the pages as an object, that relates the id of the button to the JX object
-  const [pagina, setPagina] = React.useState('home') // starting the state for wich page should be shown
-  function trocarPagina(evento) {
-    setPagina(evento.target.attributes.nomepagina.value)
-  }
-  function darkMode() {
-    setDarkOn(prevValue => { return !prevValue })
-  }
-  function activateMenu() {
-    var menuMobile = document.querySelector('#mobile_menu').classList
-    var iconeMenuMobile = document.querySelector('.mobile_menu_logo').classList
-    if (menuMobile == "") {
-      menuMobile.add('active_menu')
-      iconeMenuMobile.add('active_menu')
-    } else {
-      menuMobile.remove('active_menu')
-      iconeMenuMobile.remove('active_menu')
-    }
-  }
-  function trocarLang(evento) {
-    setLang(evento.target.id)
-  }
-  function clickMobileMenu(evento) {
-    trocarPagina(evento)
-    activateMenu()
-  }
-  var paginaAtual = paginas[pagina]
 
-  /*   return (
-      <div className={darkOn ? "App Dark" : "App White"}>
-        <nav className="navbar">
-          <ul className="lista" id="desktop_menu">
-            <li onClick={trocarPagina} nomepagina="home">{textos[lang]["Quem Sou"]}</li>
-            <li onClick={trocarPagina} nomepagina="habilidades">{textos[lang]["Experimentacao"]}</li>
-            <li onClick={trocarPagina} nomepagina="Experiencias">{textos[lang]["Historico"]}</li>
-          </ul>
-          <div id="darkModeDiv">
-            <input onChange={darkMode} id="darkmode_button" type="checkbox" checked={darkOn} />
-            <label htmlFor="darkmode_button"></label>
-          </div>
-          <div id="langDiv"> <img src={lg_ico} className="logo_header" alt="language_logo"></img>
-            <ul id="language_menu">
-              <li id="pt" onClick={trocarLang}>Português</li>
-              <li id="en" onClick={trocarLang}>English</li>
-              <li id="it" onClick={trocarLang}>Italiano</li>
-              <li id="de" onClick={trocarLang}>Deutsch</li>
-            </ul>
-          </div>
-          <div onClick={activateMenu} className="logo_header mobile_menu_logo"></div>
-          <div id="mobile_menu">
-            <ul>
-              <li onClick={clickMobileMenu} nomepagina="home">{textos[lang]["Quem Sou"]}</li>
-              <li onClick={clickMobileMenu} nomepagina="habilidades">{textos[lang]["Experimentacao"]}</li>
-              <li onClick={clickMobileMenu} nomepagina="Experiencias">{textos[lang]["Historico"]}</li>
-            </ul>
-          </div>
-  
-        </nav>
-        {paginaAtual}
-      </div>
-    ); */
-
-    const color = useColorModeValue('white', 'gray.800')
-    const { colorMode, toggleColorMode } = useColorMode()
+  const color = useColorModeValue('white', 'gray.800')
+  const { colorMode, toggleColorMode } = useColorMode()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
   return (
-    <Container maxWidth="100vw" px='0px' position='fixed'>
+    <Container maxWidth="100vw" px='0px'>
       <nav className="navbar">
-        <Flex maxWidth="100vw" direction='row'>
-        <img src={myIcon} className="logo" alt="logo"/>
-        <Button px='20px' bg={color}>AAAA</Button>
-        <Button px='20px' bg={color}>BBBB</Button>
-        <Button px='20px' bg={color}>CCCC</Button>
+        <Flex maxWidth="100vw" direction='row' className='desktop desktop_menu'>
+          <img src={myIcon} className="logo" alt="logo"/>
+          <Button px='20px' bg={color}>About Me</Button>
+          <Button px='20px' bg={color}>Skills</Button>
+          <Button px='20px' bg={color}>Portifolio</Button>
+          <Button px='20px' bg={color}>Education/Work</Button>
         </Flex>
-        <Container maxWidth="100vw" textAlign='end'>
-          <Button onClick={toggleColorMode}>{colorMode === 'light' ?<SunIcon mx='5px'/> : <MoonIcon mx='5px'/> }</Button>
-          <Button mx='5px'><img src={lg_ico} style={{'filter':colorMode === 'light' ? 'invert(1)' : 'invert(0)'}} className="logo_header" alt="language_logo"></img></Button>
-        </Container>
+        <img src={myIcon} className="logo mobile" alt="logo"/>
+        <Flex maxWidth="100vw" direction='row' textAlign='end' >
+          <Button className='desktop' bg={color} onClick={toggleColorMode}>
+            {colorMode === 'light' ?<SunIcon/> : <MoonIcon/> }
+          </Button>
+          <Button bg={color} className='desktop' >
+            <img src={lg_ico} style={{'filter':colorMode === 'light' ? 'invert(1)' : 'invert(0)'}} className="logo_header" alt="language_logo"></img>
+          </Button>
+          <Button className ='mobile burger_button' bg={color} ref={btnRef} onClick={onOpen}>
+            <HamburgerIcon/>
+          </Button>
+        </Flex>
       </nav>
-      <Flex h='200vh'>
-
+      <Flex flexDirection="column">
+        <QuemSou/>
+        <QuemSou/>
       </Flex>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+
+          <DrawerBody>
+          <Flex maxWidth="100vw" direction='column'>
+          <Button px='20px' bg={color}>About Me</Button>
+          <Button px='20px' bg={color}>Skills</Button>
+          <Button px='20px' bg={color}>Portifolio</Button>
+          <Button px='20px' bg={color}>Education/Work</Button>
+        </Flex>
+          </DrawerBody>
+
+          <DrawerFooter>
+          <Flex maxWidth="100vw" direction='row'>
+          <Button bg={color} onClick={toggleColorMode}>
+            {colorMode === 'light' ?<SunIcon/> : <MoonIcon/> }
+          </Button>
+          <Button bg={color} >
+            <img src={lg_ico} style={{'filter':colorMode === 'light' ? 'invert(1)' : 'invert(0)'}} className="logo_header" alt="language_logo"></img>
+          </Button>
+        </Flex>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Container>
   )
 }
--- src/pages/QuemSou.js
+++ src/pages/QuemSou.js
@@ -1,22 +1,34 @@
-import foto from '../images/foto.png'
+import helloSticker from '../images/hello-sticker.png'
 import textos from "../json/Textos.json"
-import { Box } from '@chakra-ui/react'
-export default function QuemSou(props) {
+import { Box, Heading, Highlight, Flex, Container, Text,useColorModeValue } from '@chakra-ui/react'
+export default function QuemSou(/* props */) {
+  const color = useColorModeValue('gray.100', 'gray.700')
+  /* var idioma =props.idioma */
+  return (
+    <Container maxWidth="100vw" justifyContent="center">
 
-  var idioma =props.idioma
-    return (
-      <div id="divInicial">
-      <img src={foto} id="profilePic" alt='foto de perfil'/>
-      <div id="profileIntro" className="textoMedio">
-        <i> Vitor Kalil</i><br />
-        <i> Front-end Webdesigner</i>
-      </div>
-      <p className="textoMedio profileText text_1">
-        {textos[idioma]["texto-inicial"]["texto_1"]}
-      </p>
-      <p className="textoMedio profileText text_2">
-        {textos[idioma]["texto-inicial"]["texto_2"]}
-      </p>
-    </div>
-    )
-  }
+      <Box maxWidth={["100vw","50vw"]} textAlign="center" m="auto">
+        <Heading lineHeight='tall' fontSize={["2xl","4xl"]}>
+          <Highlight
+            query={['learn','never stop trying']}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'pink.400' }}
+          >
+            When creating, fail or succeed,but always learn and never stop trying
+          </Highlight>
+        </Heading>
+      </Box>
+
+        <Flex direction={["column-reverse","row"]} maxWidth="80vw" bg={color} m="auto">
+          <Box p={["10px","100px"]} w="100%">
+            <Heading as="h2" size="lg" noOfLines={1}>About Me</Heading>
+            <Text fontSize='lg' p="10px">
+            {textos['de']['texto-inicial']['texto_1']}   
+            </Text>
+          </Box>
+          <Box w={["100%","80%"]} p="20px" textAlign='-webkit-center' alignSelf="center">
+            <img src={helloSticker} alt="sticker saying hello"></img>
+          </Box>
+        </Flex>
+    </Container>
+  )
+}
